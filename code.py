70774662b3fe2b2be2518f93d353495f67fa5656
kenny <kenny@sourcetonuts.com>
2020-07-06 10:17:40 -0400

    made the WS2801 led strings work with a rainbow change of hue as well as the LED white brightness on a sawtooth

--- code.py
+++ code.py
@@ -2,17 +2,30 @@ import time
 import board
 import pulseio
 import touchio
+import adafruit_ws2801
+import adafruit_fancyled.adafruit_fancyled as fancy
 
 led = pulseio.PWMOut( board.D13, frequency = 5000, duty_cycle = 128 )
 led0 = pulseio.PWMOut( board.D0, frequency = 5000, duty_cycle = 128 )
-led90 = pulseio.PWMOut( board.D2, frequency = 5000, duty_cycle = 128 )
-led180 = pulseio.PWMOut( board.D3, frequency = 5000, duty_cycle = 128 )
 
-touch = touchio.TouchIn( board.D4 )
+strip = adafruit_ws2801.WS2801( board.D3, board.D4, 7, auto_write=False)
+
+touch = touchio.TouchIn( board.A0 )
 
 throbbing = True
 goingup = True
 valduty = 5
+wason = False
+
+# across the rainbow
+grad = [ (0.0,0xFF0000), (0.33,0x00FF00), (0.67,0x0000FF), (1.0,0xFF0000) ]
+palette = fancy.expand_gradient( grad, 20 )
+
+offset = 0.25
+color = fancy.palette_lookup( palette, offset )
+strip.fill( color.pack() )
+#strip.fill( 0xFF0000 )
+strip.show()
 
 while True :
     if throbbing :
@@ -28,18 +41,18 @@ while True :
         tmpduty = valduty << 8
         led.duty_cycle = tmpduty
         led0.duty_cycle = tmpduty
-        led90.duty_cycle = tmpduty
-        led180.duty_cycle = tmpduty
+        offset = ( valduty % 256 ) / 255
+        color = fancy.palette_lookup( palette, offset )
+        strip.fill( color.pack() )
+        strip.show()
 
     # deal w/ switching modes
-    #wason = switch.value
     wason = not touch.value
     time.sleep(0.005)  # 5ms delay
-    #if not wason and not switch.value :
+
     if not wason and touch.value :
         # both was and is currently pressed, so toggle throbbing mode
         throbbing = not throbbing
         # wait until they release the button
-        #while not switch.value :
         while touch.value :
             time.sleep(0.005)  # debounce 5ms delay
