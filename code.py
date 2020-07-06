6e8419ed3a180ab5bfa5aa01f8b93f4ba5e3d9fa
kenny <kenny@sourcetonuts.com>
2020-07-06 14:45:05 -0400

    minor cleanup and removing unused

--- code.py
+++ code.py
@@ -7,7 +7,6 @@ import adafruit_fancyled.adafruit_fancyled as fancy
 
 led = pulseio.PWMOut( board.D13, frequency = 5000, duty_cycle = 128 )
 led0 = pulseio.PWMOut( board.D0, frequency = 5000, duty_cycle = 128 )
-
 strip = adafruit_ws2801.WS2801( board.D3, board.D4, 7, auto_write=False)
 
 touch = touchio.TouchIn( board.A0 )
@@ -22,10 +21,6 @@ grad = [ (0.0,0xFF0000), (0.33,0x00FF00), (0.67,0x0000FF), (1.0,0xFF0000) ]
 palette = fancy.expand_gradient( grad, 20 )
 
 offset = 0.25
-color = fancy.palette_lookup( palette, offset )
-strip.fill( color.pack() )
-#strip.fill( 0xFF0000 )
-strip.show()
 
 while True :
     if throbbing :
